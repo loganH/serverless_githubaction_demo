12793ec41560054211cf4ee613e14dbaace2fa1a
loganH <loganathan.h@gmail.com>
2020-09-21 13:14:57 -0400

    fix: deploy a new version to stage

--- src/handler/demo.js
+++ src/handler/demo.js
@@ -4,7 +4,7 @@ exports.handler = async event => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
+    body: JSON.stringify('Hello from Lambda! v1.1.1'),
   };
   return response;
 };
